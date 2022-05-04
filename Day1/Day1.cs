c93a9a6171374ecb4f46ca0ce2ca205b5104c072
loujons <loujons@kth.se>
2022-05-05 01:27:19 +0200

    Day 1

---
+++ Day1/Day1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class Day1
+    {
+        public static int First(string[] fileContent)
+        {
+            string s = fileContent[0];
+            int floor = 0;
+
+            foreach (char c in s)
+            {
+                floor += c == '(' ? 1 : -1;
+            }
+
+            return floor;
+        }
+
+        public static int Last(string[] fileContent)
+        {
+            string s = fileContent[0];
+            int floor = 0;
+            int pos = 1;
+
+            foreach (char c in s)
+            {
+                floor += c == '(' ? 1 : -1;
+
+                if (floor == -1)
+                    break;
+
+                pos++;
+            }
+
+            return pos;
+        }
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] lines = File.ReadAllLines(@"./input.in");
+
+Console.WriteLine("First [1] or Last [2]?");
+
+var choice = Console.ReadLine();
+
+if(choice == "1")
+{
+    Console.WriteLine(Day1.Day1.First(lines));
+}
+else if(choice == "2")
+{
+    Console.WriteLine(Day1.Day1.Last(lines));
+}
+
+Console.WriteLine("End of program");
