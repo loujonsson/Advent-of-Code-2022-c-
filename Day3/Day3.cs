74d686fc6dfd5ca5c188e31bca64d7e4bd784fb2
loujons <loujons@kth.se>
2022-05-08 00:37:14 +0200

    Day 3

---
+++ Day3/Day3.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Day3
+    {
+        private static Dictionary<string, bool> visitedHouses = new Dictionary<string, bool>();
+        private static string coordinates;
+        private static int numberOfHouses = 1;
+
+        internal static int First(string[] lines)
+        {
+            int x = 0;
+            int y = 0;
+
+            visitedHouses.Add("0.0", true);
+
+            foreach (char c in lines[0])
+            {
+                var arr = checkHouse(c, x, y);
+
+                x = arr[0];
+                y = arr[1];
+            }
+
+            return numberOfHouses;
+
+        }
+
+        private static int[] checkHouse(char c, int x, int y)
+        {
+          
+            if (c == '^')
+                y++;
+            else if (c == '>')
+                x++;
+            else if (c == '<')
+                x--;
+            else if (c == 'v')
+                y--;
+
+            coordinates = x.ToString() + '.' + y.ToString();
+
+            if (!visitedHouses.ContainsKey(coordinates))
+            {
+                numberOfHouses++;
+                visitedHouses.Add(coordinates, true);
+            }
+            
+
+            int[] coordinatesArray = { x, y };
+
+            return coordinatesArray;
+        }
+
+        internal static int Last(string[] lines)
+        {
+            int santaX = 0;
+            int santaY = 0;
+            int robotX = 0;
+            int robotY = 0;
+
+            visitedHouses.Add("0.0", true);
+
+            int charPlacement = 0;
+
+            foreach(char c in lines[0])
+            {
+                if (charPlacement % 2 == 0)
+                {
+                    var arr = checkHouse(c, santaX, santaY);
+
+                    santaX = arr[0];
+                    santaY = arr[1];
+                }
+                else
+                {
+                    var arr = checkHouse(c, robotX, robotY);
+
+                    robotX = arr[0];
+                    robotY = arr[1];
+                }
+                charPlacement++;
+            }
+
+            return numberOfHouses; 
+        }   
+       
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] lines = File.ReadAllLines(@"C:\Users\loujo\Documents\Advent of Code\Day3\input.in");
+
+Console.WriteLine("First [1] or Last [2]?");
+
+var choice = Console.ReadLine();
+
+if (choice == "1")
+{
+    Console.WriteLine(Day3.Day3.First(lines));
+}
+else if (choice == "2")
+{
+    Console.WriteLine(Day3.Day3.Last(lines));
+}
+
+Console.WriteLine("End of program");
